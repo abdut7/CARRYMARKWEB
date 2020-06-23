9f78e5b23005bcd36084c820217fab99cc578913
abdut7 <abdurahmant7@gmail.com>
2020-06-23 15:50:20 +0530

    Initial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,31 @@
+import { RouterModule } from '@angular/router';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DashbordComponent } from './dashbord/dashbord.component';
+import { OrdersComponent } from './orders/orders.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashbordComponent,
+    OrdersComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot([
+      {
+        path:'dashboard',
+        component:DashbordComponent
+      },
+      {
+        path:'orders',
+        component:OrdersComponent
+      }
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
